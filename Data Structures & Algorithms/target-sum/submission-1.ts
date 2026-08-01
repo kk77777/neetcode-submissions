9a2a14b6dc75fca3e4f22697536e0278c1fdac45
Kaushik Kumar Gattani <47082709+kk77777@users.noreply.github.com>
2026-08-01 11:53:09 +0530

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    findTargetSumWays(nums: number[], target: number): number {
+
+        const n=nums.length;
+
+        const memo=Array.from({length:n},()=>new Array(2000).fill(undefined));
+
+        function solve(i,curr){
+
+            if(curr===target && i==n){
+                return 1;
+            }
+
+            if(i>=n){
+                return 0;
+            }
+
+            if(memo[i][curr]!==undefined){
+                return memo[i][curr];
+            }
+
+            const add=solve(i+1,curr+nums[i]);
+            const sub=solve(i+1,curr-nums[i]);
+
+            return memo[i][curr]=add+sub;
+        }
+
+        return solve(0,0);
+    }
+}
