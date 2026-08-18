022b23dab2ebabc0448f72ecfea9eb84d0fa5086
Kaushik Kumar Gattani <47082709+kk77777@users.noreply.github.com>
2026-08-18 21:09:26 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        int carry = 0;
+        while (l1 != null && l2 != null) {
+            int val = l1.val + l2.val;
+            System.out.print("val = " + val + "\n");
+            val += carry;
+            int sum = val % 10;
+            carry = val / 10;
+            System.out.print("sum = " + sum + "\n");
+            ListNode node = new ListNode(sum);
+            curr.next = node;
+            curr = curr.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            int val = l1.val;
+            val += carry;
+            int sum = val % 10;
+            carry = val / 10;
+
+            ListNode node = new ListNode(sum);
+            curr.next = node;
+            curr = curr.next;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            int val = l2.val;
+            val += carry;
+            int sum = val % 10;
+            carry = val / 10;
+
+            ListNode node = new ListNode(sum);
+            curr.next = node;
+            curr = curr.next;
+            l2 = l2.next;
+        }
+
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+}
