b890929a2a913db97769a002e13d3f19ed598dd0
Kaushik Kumar Gattani <47082709+kk77777@users.noreply.github.com>
2026-09-01 06:23:54 +0530

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    List<List<Integer>>res=new ArrayList<>();
+
+    void solve(int[] nums,int index,List<Integer>temp){
+
+        // Base case
+        if(index>=nums.length){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        //take
+        temp.add(nums[index]);
+        
+        solve(nums,index+1,temp);
+
+
+        //untake
+        temp.removeLast();
+        while(index<nums.length-1 && nums[index]==nums[index+1]){
+            index++;
+        }
+        solve(nums,index+1,temp);
+
+        
+
+    }
+
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+
+        List<Integer>temp=new ArrayList<>();
+
+        solve(nums,0,temp);
+
+        return res;
+    }
+}
